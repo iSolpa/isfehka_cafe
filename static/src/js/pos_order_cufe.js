e2218c6dcce16938ede10b94b0a69cc7b796b48d
iS-AJ <alfredo@isolpa.com>
2025-07-28 07:26:11 -0500

    pos data sync for cafe handling

--- static/src/js/pos_order_cufe.js
+++ static/src/js/pos_order_cufe.js
@@ -1,74 +1,54 @@
 /** @odoo-module */
 
-import { PosOrder } from "@point_of_sale/app/models/pos_order";
+import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
 import { patch } from "@web/core/utils/patch";
+import { usePos } from "@point_of_sale/app/store/pos_hook";
 
-console.log("[ISFEHKA CAFE] Loading model patches for Odoo 18");
+console.log("[ISFEHKA CAFE] Loading receipt component patch");
 
-patch(PosOrder.prototype, {
-    export_as_JSON() {
-        const json = super.export_as_JSON(...arguments);
-        console.log("[ISFEHKA CAFE] Exporting order as JSON:", {
-            orderName: this.name,
-            hasCufe: !!this.hka_cufe
-        });
+patch(OrderReceipt.prototype, {
+    setup() {
+        super.setup();
+        this.pos = usePos();
         
-        if (this.hka_cufe) {
-            console.log("[ISFEHKA CAFE] Including CUFE data in export for order:", this.name);
-            json.hka_cufe = this.hka_cufe;
-            json.hka_cufe_qr = this.hka_cufe_qr;
-        }
-        return json;
+        console.log("[ISFEHKA CAFE] Setting up CAFE receipt component");
     },
-
-    init_from_JSON(json) {
-        console.log("[ISFEHKA CAFE] Initializing order from JSON:", {
-            orderName: json.name,
-            hasCufe: !!json.hka_cufe
-        });
+    
+    get hkaCufe() {
+        // Get CUFE from current order or reprint data
+        const isReprint = this.props.data?.isReprint === true;
         
-        super.init_from_JSON(...arguments);
-        if (json.hka_cufe) {
-            console.log("[ISFEHKA CAFE] Found CUFE data in JSON for order:", json.name);
-            this.hka_cufe = json.hka_cufe;
-            this.hka_cufe_qr = json.hka_cufe_qr;
-        }
-    },
-
-    export_for_printing() {
-        const result = super.export_for_printing(...arguments);
-        
-        // Add CUFE data to headerData for template access
-        if (this.hka_cufe) {
-            result.headerData = result.headerData || {};
-            result.headerData.hka_cufe = this.hka_cufe;
-            
-            // Handle QR code - check if it's official HKA QR or custom generated
-            if (this.hka_cufe_qr) {
-                // If it's already a data URL, use as is; otherwise add prefix
-                if (this.hka_cufe_qr.startsWith('data:')) {
-                    result.headerData.hka_cufe_qr = this.hka_cufe_qr;
-                } else {
-                    result.headerData.hka_cufe_qr = `data:image/png;base64,${this.hka_cufe_qr}`;
-                }
-            }
-            
-            console.log("[ISFEHKA CAFE] Added CUFE to headerData for order:", this.name, {
-                cufe: this.hka_cufe,
-                qrLength: this.hka_cufe_qr ? this.hka_cufe_qr.length : 0,
-                qrPrefix: this.hka_cufe_qr ? this.hka_cufe_qr.substring(0, 50) : 'none'
-            });
+        if (isReprint && this.props.data?.hka_cufe) {
+            console.log("[ISFEHKA CAFE] Getting CUFE from reprint data:", this.props.data.hka_cufe);
+            return this.props.data.hka_cufe;
         }
         
-        console.log("[ISFEHKA CAFE] Export for printing:", {
-            orderName: this.name,
-            hasCufe: !!this.hka_cufe,
-            headerHasCufe: !!(result.headerData && result.headerData.hka_cufe),
-            hasQrCode: !!(result.headerData && result.headerData.hka_cufe_qr),
-            qrDataType: result.headerData && result.headerData.hka_cufe_qr ? 
-                (result.headerData.hka_cufe_qr.startsWith('data:') ? 'dataURL' : 'raw') : 'none'
-        });
+        const currentOrder = this.pos?.get_order();
+        if (currentOrder?.hka_cufe) {
+            console.log("[ISFEHKA CAFE] Getting CUFE from current order:", currentOrder.hka_cufe);
+            return currentOrder.hka_cufe;
+        }
         
-        return result;
+        console.log("[ISFEHKA CAFE] No CUFE found");
+        return null;
     },
+    
+    get hkaCufeQr() {
+        // Get QR from current order or reprint data
+        const isReprint = this.props.data?.isReprint === true;
+        
+        if (isReprint && this.props.data?.hka_cufe_qr) {
+            console.log("[ISFEHKA CAFE] Getting QR from reprint data");
+            return this.props.data.hka_cufe_qr;
+        }
+        
+        const currentOrder = this.pos?.get_order();
+        if (currentOrder?.hka_cufe_qr) {
+            console.log("[ISFEHKA CAFE] Getting QR from current order");
+            return currentOrder.hka_cufe_qr;
+        }
+        
+        console.log("[ISFEHKA CAFE] No QR found");
+        return null;
+    }
 });
