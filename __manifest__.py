62c03a0bfd2d3c94d9291ecb06792ee9dbec3fe4
iS-AJ <alfredo@isolpa.com>
2025-11-14 11:29:40 -0500

    17 refactor fixes

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ISFEHKA CAFE - Panama DGI Receipt Extension',
-    'version': '17.0.1.0.5',
+    'version': '17.0.1.0.6',
     'category': 'Accounting/Localizations',
     'summary': 'Panama DGI CAFE (Comprobante Auxiliar de Factura Electrónica) generation for receipts',
     'description': """
@@ -33,6 +33,7 @@
     'assets': {
         'point_of_sale._assets_pos': [
             'isfehka_cafe/static/src/js/pos_order_cufe.js',
+            'isfehka_cafe/static/src/js/receipt_screen_cafe.js',
             'isfehka_cafe/static/src/xml/pos_ticket_cafe.xml',
         ],
     },
---
+++ static/src/js/receipt_screen_cafe.js
@@ -0,0 +1,39 @@
+/** @odoo-module */
+
+import { ReceiptScreen } from "@point_of_sale/app/screens/receipt/receipt_screen";
+import { patch } from "@web/core/utils/patch";
+import { onWillStart } from "@odoo/owl";
+
+// Ensure CUFE fields are present on the Order before the receipt renders.
+patch(ReceiptScreen.prototype, {
+    setup() {
+        super.setup();
+        onWillStart(async () => {
+            try {
+                const order = this.currentOrder;
+                if (order && order.server_id && !order.hka_cufe) {
+                    const data = await this.env.services.orm.call(
+                        "pos.order",
+                        "read",
+                        [[order.server_id], [
+                            "hka_cufe",
+                            "hka_cufe_qr",
+                            "hka_nro_protocolo_autorizacion",
+                            "hka_fecha_recepcion_dgi",
+                            "hka_tipo_documento",
+                        ]]
+                    );
+                    if (data && data.length && data[0].hka_cufe) {
+                        order.hka_cufe = data[0].hka_cufe;
+                        order.hka_cufe_qr = data[0].hka_cufe_qr;
+                        order.hka_nro_protocolo_autorizacion = data[0].hka_nro_protocolo_autorizacion;
+                        order.hka_fecha_recepcion_dgi = data[0].hka_fecha_recepcion_dgi;
+                        order.hka_tipo_documento = data[0].hka_tipo_documento;
+                    }
+                }
+            } catch (e) {
+                console.warn("[ISFEHKA CAFE] Could not preload CUFE for receipt:", e);
+            }
+        });
+    },
+});
